0a6bbd61d6fd21a3e5bb102a887ba52a3c81b778
Wiktor Obłój <w65555@student.wsiz.edu.pl>
2022-10-20 12:55:55 +0200

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Lab1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get(int value)
+        {
+            var calculateInstance = new Calculate(value);
+            calculateInstance.Increase(15);
+            return calculateInstance.GetValue();
+        }
+    }
+}
---
+++ Services/Calculate.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab1.Services
+{
+    public class Calculate
+    {
+        public Calculate(int value)
+        {
+            Value = value;
+        }
+
+        private int Value { get; set; }
+
+        public void Increase(int value)
+        {
+            Value += value;
+        }
+
+        public void Decrease(int value)
+        {
+            Value -= value;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+    }
+}
